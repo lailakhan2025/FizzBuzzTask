b2890a3d8ef0ac425771951da4bff83ba9cf1ce5
Laila Khan <lailatariqkhan@gmail.com>
2025-02-24 11:59:11 +0000

    FizzBuzz Part 1-3 completed

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Reflection.Metadata;
+
+namespace FizzBuzzTask
+{
+    class FizzBuzzTask
+    {
+        public static void Main(string[] args)
+        {
+            GetFizzBuzz();
+        }
+
+        public static void GetFizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
